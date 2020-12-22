0e01f163b1ae34e825183ba1c666797a5caa1e81
unknown <mittapabt2536@gmail.com>
2020-12-22 14:45:28 +0700

    up project

---
+++ katexoxo-favicon/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+/**
+ * Plugin Name: Kate xo Favicon
+ * Description: Add Favicon to website
+ * Author: Kate xo Developer
+ * Version: 1.0
+ */
+
+if(!defined('ABSPATH')){
+    exit;
+}
+
+
+ ?>
+
+    <link rel="icon" href="https://uat.katexoxo.com/wp-content/uploads/2020/05/cropped-cropped-katexoxo-favicon-v3-1-32x32.png" sizes="32x32" /> 
+    <link rel="icon" href="https://uat.katexoxo.com/wp-content/uploads/2020/05/cropped-cropped-katexoxo-favicon-v3-1-192x192.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://uat.katexoxo.com/wp-content/uploads/2020/05/cropped-cropped-katexoxo-favicon-v3-1-180x180.png" />
